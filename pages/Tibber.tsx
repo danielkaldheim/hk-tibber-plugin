d44bc333afc437f35f8b93075e56b0002e5727ef
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2020-06-24 15:50:19 +0200

    Updated package json, added assets

--- pages/Tibber.tsx
+++ pages/Tibber.tsx
@@ -4,6 +4,7 @@ import { H1, View, Container } from 'native-base';
 import SetupTibber from './SetupTibber';
 import { StyleSheet } from 'react-native';
 import { typography } from 'styles';
+import TibberLogo from '../assets/TibberLogo';
 
 const styles = StyleSheet.create({
   title: {
@@ -17,7 +18,9 @@ const Tibber = (): JSX.Element => {
     <TibberProvider>
       <Container style={{ padding: 20 }}>
         <View>
-          <H1 style={styles.title}>Tibber</H1>
+          <View style={{ marginBottom: 20 }}>
+            <TibberLogo />
+          </View>
           <SetupTibber />
         </View>
       </Container>
